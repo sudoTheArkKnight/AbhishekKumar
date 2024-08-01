5be4e632e60d3dadae2dd3bd6bc339117f84e5b3
Abhishek Kumar <Abhishekkumaronroute@gmail.com>
2024-08-01 22:29:48 +0530

    Finalizing website, barring the certificates section and some in project section. Ready for Publication.

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,12 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     const aboutCard = document.querySelector('.about-card');
-    const aboutDetails = document.querySelector('.about-details');
-
-    aboutCard.addEventListener('click', function () {
-        if (aboutDetails.style.display === 'none' || aboutDetails.style.display === '') {
-            aboutDetails.style.display = 'block';
-        } else {
-            aboutDetails.style.display = 'none';
-        }
+    
+    aboutCard.addEventListener('click', function() {
+        aboutCard.classList.toggle('expanded');
     });
 });
